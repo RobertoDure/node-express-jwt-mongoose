857314ef57c51e37f20e215b96ab4b27c87070bb
Roberto Duré <robertodure1990@gmail.com>
2019-01-18 18:15:11 +0000

    Update data-config.js

--- database/data-config.js
+++ database/data-config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://root:hall9000@ds225382.mlab.com:25382/roberto-dure-db');
+mongoose.connect('mongodb://root:root@ds225382.mlab.com:25382/roberto-dure-db');
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
